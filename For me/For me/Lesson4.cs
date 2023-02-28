8700bef8cc64425543be029ef197f2b71f5cffdf
Anastasiya Zayats <Nastyabonybelay@gmail.com>
2023-02-28 22:25:38 +0300

    “add new class”

---
+++ For me/For me/Lesson4.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace For_me
+{
+	public class Lesson4
+	{
+        public static void Task1()
+        {
+            string s1 = "Hello";
+            Console.WriteLine(s1);
+
+            Console.WriteLine(s1[0]);
+        }
+
+    }
+}
+
--- For me/For me/Program.cs
+++ For me/For me/Program.cs
@@ -4,6 +4,9 @@ internal class Programm
 {
     static void Main(string[] args)
     {
+        For_me.Lesson4.Task1();
+
+
         //int[] numbers = { 5, -6, 7, 20 };
 
         ////Console.WriteLine(numbers[0]);
@@ -18,30 +21,28 @@ internal class Programm
         //}
 
         // 1 2 3
-        int[] numbers = new int[3];
-        Random random = new Random();
+        //int[] numbers = new int[3];
+        //Random random = new Random();
 
-        // 1 2 3
-        // 4 5 6
-        // 7 8 9
+        //// 1 2 3
+        //// 4 5 6
+        //// 7 8 9
 
-        Console.WriteLine($"Test check");
-        Console.WriteLine($"Test check 2");
+        //Console.WriteLine($"Test check");
+        //Console.WriteLine($"Test check 2");
 
-        int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+        //int[,] matrix = { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
 
-        for (int i = 0; i < 3; i++)
-        {
-            for (int j = 0; j < 3; j++)
-            {
-                Console.Write(matrix[i, j] + " ");
-            }
+        //for (int i = 0; i < 3; i++)
+        //{
+        //    for (int j = 0; j < 3; j++)
+        //    {
+        //        Console.Write(matrix[i, j] + " ");
+        //    }
 
-            Console.WriteLine();
-        }
+        //    Console.WriteLine();
+        
     }
-
-
 }
 
 
